969264df73e46ae8807c39e3414a707917a321fd
Annaliskirwa <annaliskirwa@gmail.com>
2022-06-09 15:11:08 +0300

    created the account model

--- src/main/java/com/kcbgroup/main/kcbcustomercreate/model/Account.java
+++ src/main/java/com/kcbgroup/main/kcbcustomercreate/model/Account.java
@@ -1,4 +1,20 @@
 package com.kcbgroup.main.kcbcustomercreate.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Account {
+    private long id;
+    private String accountNumber;
+    private BigDecimal availableBalance;
+    private BigDecimal actualBalance;
+
 }
