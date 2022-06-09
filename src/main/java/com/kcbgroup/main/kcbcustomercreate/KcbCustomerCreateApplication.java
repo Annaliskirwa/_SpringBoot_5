c98decd37fe386f161b03be7ad6afefc429eb228
Annaliskirwa <annaliskirwa@gmail.com>
2022-06-09 13:01:33 +0300

    initliazed the project

---
+++ src/main/java/com/kcbgroup/main/kcbcustomercreate/KcbCustomerCreateApplication.java
@@ -0,0 +1,13 @@
+package com.kcbgroup.main.kcbcustomercreate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcbCustomerCreateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcbCustomerCreateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kcbgroup/main/kcbcustomercreate/KcbCustomerCreateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcbgroup.main.kcbcustomercreate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcbCustomerCreateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
