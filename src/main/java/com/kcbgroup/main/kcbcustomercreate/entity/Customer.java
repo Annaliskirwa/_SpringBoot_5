7f473bde3051891cc05ae332259533421462a51a
Annaliskirwa <annaliskirwa@gmail.com>
2022-06-09 20:59:02 +0300

    created the customer entity

--- src/main/java/com/kcbgroup/main/kcbcustomercreate/entity/Customer.java
+++ src/main/java/com/kcbgroup/main/kcbcustomercreate/entity/Customer.java
@@ -1,4 +1,82 @@
 package com.kcbgroup.main.kcbcustomercreate.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "KEN20950_CUSTOMER")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long customerId;
+
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+
+    @Column(name = "identificationNumber", nullable = false)
+    private String identificationNumber;
+
+    @Column(name = "phoneNumber", nullable = false)
+    private String phoneNumber;
+
+//    Getter and Setter
+    public long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public void setIdentificationNumber(String identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+//    To string
+
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", identificationNumber='" + identificationNumber + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
