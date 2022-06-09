23ce30353e2560d1b23be0e9ba94a9ea7a21efe6
Annaliskirwa <annaliskirwa@gmail.com>
2022-06-09 16:11:22 +0300

    created transaction model

--- src/main/java/com/kcbgroup/main/kcbcustomercreate/model/Transactions.java
+++ src/main/java/com/kcbgroup/main/kcbcustomercreate/model/Transactions.java
@@ -1,4 +1,19 @@
 package com.kcbgroup.main.kcbcustomercreate.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Transactions {
+    private long transactionsId;
+    private BigDecimal amount;
+    private String referenceNumber;
+    private String transactionCode;
 }
