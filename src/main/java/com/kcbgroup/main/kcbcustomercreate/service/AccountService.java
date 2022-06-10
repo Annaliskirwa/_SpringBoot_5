c615736d5940412b967cff7c4f83f1f50685460d
Annaliskirwa <annaliskirwa@gmail.com>
2022-06-10 10:49:12 +0300

    account service interface

--- src/main/java/com/kcbgroup/main/kcbcustomercreate/service/AccountService.java
+++ src/main/java/com/kcbgroup/main/kcbcustomercreate/service/AccountService.java
@@ -1,4 +1,15 @@
 package com.kcbgroup.main.kcbcustomercreate.service;
 
+import com.kcbgroup.main.kcbcustomercreate.entity.Account;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface AccountService {
+    List<Account> getAllAccounts();
+    Account createAccount(Account account);
+    Account updateAccount(Long accountId, Account account);
+    String deleteAccount(Long accountId);
+    Account findByAccountNumber(String accountNumber);
 }
