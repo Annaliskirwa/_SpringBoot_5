c362b08be9e8b1c04db17425d900e802ebbe5641
Annaliskirwa <annaliskirwa@gmail.com>
2022-06-11 22:40:48 +0300

    listing all customers;dto

--- src/main/java/com/kcbgroup/main/kcbcustomercreate/controller/CustomerController.java
+++ src/main/java/com/kcbgroup/main/kcbcustomercreate/controller/CustomerController.java
@@ -1,13 +1,26 @@
 package com.kcbgroup.main.kcbcustomercreate.controller;
 
+import com.kcbgroup.main.kcbcustomercreate.dto.CustomerDto;
+import com.kcbgroup.main.kcbcustomercreate.repository.CustomerRepository;
 import com.kcbgroup.main.kcbcustomercreate.service.CustomerService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("kcbCustomerCreate/customer")
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
+    private ModelMapper modelMapper;
+
+    @GetMapping
+    public List<CustomerDto> getAllCustomers(){
+        return customerService.getAllCustomers().stream().map(customer -> modelMapper.map(customer, CustomerDto.class)).collect(Collectors.toList());
+    }
 }
