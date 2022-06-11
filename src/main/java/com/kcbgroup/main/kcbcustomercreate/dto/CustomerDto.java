da6b615c74a9cff620ae5aa35cc29798c5d2c577
Annaliskirwa <annaliskirwa@gmail.com>
2022-06-11 21:48:38 +0300

    created the customer dto

---
+++ src/main/java/com/kcbgroup/main/kcbcustomercreate/dto/CustomerDto.java
@@ -0,0 +1,11 @@
+package com.kcbgroup.main.kcbcustomercreate.dto;
+
+import lombok.Data;
+
+@Data
+public class CustomerDto {
+    private String firstName;
+    private String lastName;
+    private String identificationNumber;
+    private String phoneNumber;
+}
